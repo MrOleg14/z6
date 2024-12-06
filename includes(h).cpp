415bf5e7f7041d4ae29a8110fc7f659080aee1ec
MrOleg14 <145416891+MrOleg14@users.noreply.github.com>
2024-12-06 10:19:07 +0500

    Create includes(h).cpp

---
+++ includes(h).cpp
@@ -0,0 +1,15 @@
+#ifndef INCS
+#define INCS
+
+#include <iostream>  // данные выводим
+
+#include <string>
+#include <initializer_list>  // массив, но список {} передаем список параметров
+#include <vector>
+#include <utility>  // pair 
+
+// замены
+#define _clstyping template<typename _T>  
+#define strbuff std::vector<std::string> 
+
+#endif
